6002e61764430037233628a2b19ae1ac3cf53646
Neverlanders <vanhoutte.tom@gmail.com>
2021-10-21 10:25:19 +0200

    push

--- js11/js/script.js
+++ js11/js/script.js
@@ -1,4 +1,19 @@
-document.querySelector("#id-checkbox").addEventListener("click", function(event){
-    document.getElementById("resultaat").innerHTML += "Sorry!<code>preventDefault()</code> verbied u dit aan te vinken " + "<br>";
-    event.preventDefault();
-}, false);
+//close button en toevoegen aan ieder li item
+var myNodeList = document.getElementsByTagName("LI");
+var i;
+for(i=0;i< myNodeList.length;i++){
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+//click close button en verberg item
+var close = document.getElementsByClassName("close");
+var i;
+for(i=0;i< close.length;i++) {
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
